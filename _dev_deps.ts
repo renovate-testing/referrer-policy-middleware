c6d2a652db19bc62a6ad76c78d767eaddd6b731e
Tomoki Miyauchi <development.operation.6.6.6@gmail.com>
2023-04-02 10:56:02 +0900

    chore: add lock file

--- _dev_deps.ts
+++ _dev_deps.ts
@@ -3,5 +3,5 @@ export {
   assertEquals,
 } from "https://deno.land/std@0.181.0/testing/asserts.ts";
 export { describe, it } from "https://deno.land/std@0.181.0/testing/bdd.ts";
-export { equalsResponse } from "https://deno.land/x/http_utils@1.0.0-beta.15/response.ts";
+export { equalsResponse } from "https://deno.land/x/http_utils@1.0.0/response.ts";
 export { Header } from "./constants.ts";
